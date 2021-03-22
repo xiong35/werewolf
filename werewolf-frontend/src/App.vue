7b68e33860740915cf40e192feeadc0cc7e14786
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-03-22 14:33:07 +0800

    init projects

---
+++ werewolf-frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld message="Hello Vue 3.0 + Vite" />
+</template>
+
+<script lang="ts">
+  import HelloWorld from "./components/HelloWorld.vue";
+
+  import { defineComponent } from "vue";
+
+  const Component = defineComponent({
+    name: "App",
+    components: {
+      HelloWorld,
+    },
+    // 已启用类型推断
+  });
+
+  export default Component;
+</script>
---
+++ werewolf-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ result }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  const Component = defineComponent({
+    name: "HelloWorld",
+    props: {
+      message: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        count: 0,
+      };
+    },
+    setup(props) {
+      const result = props.message.split(""); // 正确, 'message' 被声明为字符串
+      return { result };
+    },
+  });
+  export default Component;
+</script>
