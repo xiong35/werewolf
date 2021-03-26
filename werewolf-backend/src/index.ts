6d771d7e0c044ff87653d7999f0b8d1fc389be46
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-03-26 11:37:03 +0800

    be: add error handling

--- werewolf-backend/src/index.ts
+++ werewolf-backend/src/index.ts
@@ -4,17 +4,24 @@ import * as KoaBody from "koa-body";
 import * as cors from "@koa/cors";
 
 import router from "./routes";
+import handleError from "./middleware/handleError";
 
 mongoose.connect("mongodb://localhost/werewolf", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const app = new Koa();
+const app = new Koa<
+  { isKnownError: Boolean },
+  { error: (status: number, msg: string) => void }
+>();
 
 app
+  .use(handleError)
   .use(cors())
   .use(KoaBody())
+
   .use(router.routes())
   .use(router.allowedMethods())
+
   .listen(3030);
