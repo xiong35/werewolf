cf761a73a3a5575878d178c76ea4b56666a53d60
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-03-28 09:33:21 +0800

    fe: add css normalize

--- werewolf-frontend/src/main.js
+++ werewolf-frontend/src/main.js
@@ -1,9 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./t.vue";
+import router from "./router";
 
-import './index.css'
+import "./normalize.css";
+import "./index.css";
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+createApp(App).use(router).mount("#app");
