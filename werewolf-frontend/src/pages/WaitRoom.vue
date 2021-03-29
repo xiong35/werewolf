bf8601296e16c08aa398e4da8f46255bb0dabad7
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-03-29 19:25:07 +0800

    fe: import qr code

--- werewolf-frontend/src/pages/WaitRoom.vue
+++ werewolf-frontend/src/pages/WaitRoom.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="waitroom"></div>
+  <div class="waitroom">
+    <div id="qr-code"></div>
+  </div>
 </template>
 
 <script lang="ts">
   import { defineComponent } from "vue";
 
+  import QRCode from "easyqrcodejs";
+
   const WaitRoom = defineComponent({
     name: "WaitRoom",
     components: {},
+    mounted() {
+      new QRCode(document.getElementById("qr-code"), {
+        text: "https://cssscript.com",
+        logo: "/wolf.png",
+        logoWidth: 20,
+        logoHeight: 20,
+        // logoBackgroundColor: "pink",
+        logoBackgroundTransparent: true,
+        width: 100,
+        height: 100,
+      });
+    },
+
     setup(props) {},
   });
 
