09515ea82967e8d4e0fa4f47456df43f09e8e1d0
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-04-01 11:13:51 +0800

    be: implement get characterStatus

--- werewolf-backend/src/handlers/http/playCharacterStatus.ts
+++ werewolf-backend/src/handlers/http/playCharacterStatus.ts
@@ -1,16 +1,27 @@
 import { Middleware } from "koa";
-import Room from "../../models/RoomModel";
+import { listAllOfNumber } from "../../models/RoomModel";
 import Player from "../../models/PlayerModel";
 
 import { CharacterStatusResponse } from "../../../../werewolf-frontend/shared/httpMsg/CharacterStatusMsg";
 
 const characterStatus: Middleware = async (ctx, next) => {
-  // const ret: CharacterStatusResponse = {
-  //   status: 200,
-  //   msg: "ok",
-  //   data: {},
-  // };
-  // ctx.body = ret;
+  const token = ctx.get("Token");
+  const roomNumber = ctx.get("RoomNumber");
+
+  const curPlayer = await Player.findOne({ _id: token });
+
+  const players = await listAllOfNumber(roomNumber);
+
+  const ret: CharacterStatusResponse = {
+    status: 200,
+    msg: "ok",
+    data: {
+      curCharacter: curPlayer.character,
+      curStatus: curPlayer.characterStatus,
+      players,
+    },
+  };
+  ctx.body = ret;
 };
 
 export default characterStatus;
