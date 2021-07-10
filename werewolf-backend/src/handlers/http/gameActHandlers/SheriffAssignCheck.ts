769697af253c450d7278bfc8f039db09e3f9d563
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-07-10 14:02:32 +0800

    be: add status: SheriffAssignCheck, add no hunter check

---
+++ werewolf-backend/src/handlers/http/gameActHandlers/SheriffAssignCheck.ts
@@ -0,0 +1,30 @@
+import { Context } from "koa";
+import io from "src";
+import { Player } from "src/models/PlayerModel";
+import { Room } from "src/models/RoomModel";
+import { getVoteResult } from "src/utils/getVoteResult";
+
+import { GameStatus, TIMEOUT } from "../../../../../werewolf-frontend/shared/GameDefs";
+import { index } from "../../../../../werewolf-frontend/shared/ModelDefs";
+import { Events } from "../../../../../werewolf-frontend/shared/WSEvents";
+import { ChangeStatusMsg } from "../../../../../werewolf-frontend/shared/WSMsg/ChangeStatus";
+import { DieCheckHandler, GameActHandler, Response } from "./";
+
+export const SheriffAssignCheckHandler: DieCheckHandler = {
+  curStatus: GameStatus.SHERIFF_ASSIGN_CHECK,
+
+  async handleHttpInTheState(
+    room: Room,
+    player: Player,
+    target: index,
+    ctx: Context
+  ) {
+    return {
+      status: 200,
+      msg: "ok",
+      data: { target },
+    };
+  },
+
+  async endOfState(room: Room) {},
+};
