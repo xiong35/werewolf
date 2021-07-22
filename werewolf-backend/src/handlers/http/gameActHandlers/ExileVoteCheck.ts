d41ac7eb2da566bd1552b6dd883554372276593e
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-07-22 14:16:07 +0800

    be: add exile vote chewck handler

--- werewolf-backend/src/handlers/http/gameActHandlers/ExileVoteCheck.ts
+++ werewolf-backend/src/handlers/http/gameActHandlers/ExileVoteCheck.ts
@@ -4,14 +4,11 @@ import { Player } from "src/models/PlayerModel";
 import { Room } from "src/models/RoomModel";
 import { getVoteResult } from "src/utils/getVoteResult";
 
-import {
-  GameStatus,
-  TIMEOUT,
-} from "../../../../../werewolf-frontend/shared/GameDefs";
+import { GameStatus, TIMEOUT } from "../../../../../werewolf-frontend/shared/GameDefs";
 import { index } from "../../../../../werewolf-frontend/shared/ModelDefs";
 import { Events } from "../../../../../werewolf-frontend/shared/WSEvents";
 import { ChangeStatusMsg } from "../../../../../werewolf-frontend/shared/WSMsg/ChangeStatus";
-import { GameActHandler, Response } from "./";
+import { GameActHandler, Response, startCurrentState, status2Handler } from "./";
 
 export const ExileVoteCheckHandler: GameActHandler = {
   curStatus: GameStatus.EXILE_VOTE_CHECK,
@@ -28,11 +25,16 @@ export const ExileVoteCheckHandler: GameActHandler = {
       data: { target },
     };
   },
-
-  async endOfState(room: Room) {},
-
   /**
    * @param nextState 在确认完结果后进入哪个状态
    */
-  startOfState: function (room: Room, nextState: GameStatus) {},
+  startOfState: function (room: Room, nextState: GameStatus) {
+    startCurrentState(this, room, nextState);
+  },
+  /**
+   * @param nextState 在确认完结果后进入哪个状态
+   */
+  async endOfState(room: Room, nextState: GameStatus) {
+    status2Handler[nextState].startOfState(room);
+  },
 };
