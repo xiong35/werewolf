dbdc1a9c7cfc95e3d1e675fea31832cb92f69fd7
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-08-03 12:00:55 +0800

    fe: finish review page

--- werewolf-frontend/src/pages/Review.vue
+++ werewolf-frontend/src/pages/Review.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="review">
-    <div class="title">游戏结束</div>
+    <div class="title">游戏记录</div>
     <div class="events" v-if="records.length">
-      {{ records }}
+      <div
+        class="record-item"
+        v-for="recordBrief in records"
+        :key="recordBrief.time"
+        @click="
+          $router.push({
+            name: 'review-detail',
+            query: {
+              roomNumber: recordBrief.roomNumber,
+              time: recordBrief.time,
+            },
+          })
+        "
+      >
+        <div class="room-number info">
+          <span class="key">房间: </span>
+          {{ recordBrief.roomNumber }}
+        </div>
+        <div class="time info">
+          <span class="key">时间: </span>
+          {{ new Date(recordBrief.time).toLocaleString() }}
+        </div>
+      </div>
     </div>
 
-    <div class="place-holder" v-else>
+    <div class="placeholder" v-else>
       什么都没有呢
     </div>
+
+    <Btn
+      :onClick="() => $router.push({ name: 'home' })"
+      content="返回主页"
+    ></Btn>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent, onMounted, ref } from "vue";
+  import { computed, defineComponent, onMounted, ref } from "vue";
+  import Btn from "../components/Btn.vue";
+
   import PlayEventList from "../components/PlayEventList.vue";
 
   import { showDialog } from "../reactivity/dialog";
@@ -21,7 +50,7 @@
 
   const ReviewPage = defineComponent({
     name: "ReviewPage",
-    components: { PlayEventList },
+    components: { PlayEventList, Btn },
     setup(props) {
       const records = useAllRecords();
 
@@ -34,4 +63,34 @@
 
   export default ReviewPage;
 </script>
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+  .review {
+    text-align: center;
+    .title {
+      font-size: 2rem;
+      font-weight: bold;
+      padding: 1.5rem;
+    }
+    .events {
+      margin: 0 auto 3rem;
+      max-width: 95vw;
+
+      .record-item {
+        text-align: left;
+        padding: 0.4rem;
+        cursor: pointer;
+        .info {
+          margin: 0.2rem;
+        }
+        .key {
+          font-weight: bold;
+        }
+      }
+    }
+    .placeholder {
+      margin: 2rem 0 4rem;
+      opacity: 0.5;
+    }
+  }
+</style>
